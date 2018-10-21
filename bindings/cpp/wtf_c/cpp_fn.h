5e3fe70074a1a695ad5539eaab364c5326559e7a
Derrick Dominic <ddominic@waymo.com>
2018-10-20 20:20:02 -0700

    Removing unnecessary and generated files from wtf_c/

--- bindings/cpp/wtf_c/cpp_fn.h
+++
@@ -1,8 +0,0 @@
-#ifndef CPP_FN
-#define CPP_FN
-
-#include <string>
-
-void cpp_fn(const std::string& str);
-
-#endif  // CPP_FN
--- bindings/cpp/wtf_c/wtf_c.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void c_fn(const char* str);
-
-int main(void) {
-  c_fn("test string");
-  return 0;
-}
