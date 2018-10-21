5a42ffc36e601edf70b81f429bc6acbefdd096d3
Derrick Dominic <ddominic@waymo.com>
2018-10-20 20:42:40 -0700

    Basic file structure and working Makefile for test_wtf_c/

---
+++ bindings/cpp/test_wtf_c/wtf_c.h
@@ -0,0 +1,12 @@
+#ifndef WTF_C_H_
+#define WTF_C_H_
+
+void wtf_scope0(const char* name);
+
+void wtf_event0(const char* name);
+
+void wtf_thread_enable(const char* name);
+
+int wtf_save_to_file(const char* filename);
+
+#endif  // WTF_C_H_
